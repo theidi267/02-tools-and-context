989941d0bdb7f5bb1de1c204fc8aeeb96f1f17fc
theidi267 <timea.heidenreich@gmail.com>
2018-05-24 15:34:43 -0700

    linter added

--- lab-timea/lib/list.js
+++ lab-timea/lib/list.js
@@ -1,64 +1,37 @@
 'use strict';
 
 class List {
-    constructor() {
-      this.length = 0;
-    }
-  
-    push(item) {
-      this[this.length++] = item;
-    }
-  
-    pop() {
-      let item = this[this.length-1];
-      delete this[this.length-1];
-      this.length--;
-      return item;
-    }
-
-    shift() {
-      let item
-
-
-
-
-    }
-
-
-
-
-    unshift()
-
-
-  
-    forEach(callback) {
-      if ( typeof callback !== 'function' ) { throw new Error('Callback Required'); }
-      for (let i = 0; i <= this.length - 1; i++) {
-        callback(this[i], i);
-      }
-    }
-  
-    map(callback) {
-      if ( typeof callback !== 'function' ) { return undefined; }
-      let result = new List();
-      for (let i = 0; i <= this.length - 1; i++) {
-        result.push(callback(this[i], i));
-      }
-      return result;
-    }
-
-    filter()
-
-
-
-    reduce()
-
-    slice()
-
-    splice()
-
-
-  
+  constructor() {
+    this.length = 0;
   }
   
-  module.exports = List;
+  push(item) {
+    this[this.length++] = item;
+  }
+  
+  pop() {
+    let item = this[this.length-1];
+    delete this[this.length-1];
+    this.length--;
+    return item;
+  }
+
+  forEach(callback) {
+    if ( typeof callback !== 'function' ) { throw new Error('Callback Required'); }
+    for (let i = 0; i <= this.length - 1; i++) {
+      callback(this[i], i);
+    }
+  }
+  
+  map(callback) {
+    if ( typeof callback !== 'function' ) { return undefined; }
+    let result = new List();
+    for (let i = 0; i <= this.length - 1; i++) {
+      result.push(callback(this[i], i));
+    }
+    return result;
+  }
+
+}
+  
+module.exports = List;
